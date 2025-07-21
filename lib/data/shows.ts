1b8570fe96ced1d92b0dbae600033cf8da79fd94
Oleksandr Dorokhov <alex.dorokhov.mail@gmail.com>
2025-07-21 16:09:46 +0300

    Data added in shows.ts

--- lib/data/shows.ts
+++ lib/data/shows.ts
@@ -32,5 +32,77 @@ export const shows: ShowType[] = [
         name: 'Spring And Wings Art Show',
         src: '/images/show-4.jpg',
         display: 'reversed'
+    },
+    {
+        year: 2025,
+        city: 'Kyiv',
+        country: 'Ukraine',
+        name: 'Light of Color and Summer Vibes',
+        src: '/images/show-5.jpg',
+        display: 'normal'
+    },
+    {
+        year: 2025,
+        city: 'Kyiv',
+        country: 'Ukraine',
+        name: 'Together for dream',
+        src: '/images/show-6.jpg',
+        display: 'reversed'
+    },
+    {
+        year: 2025,
+        city: 'Odesa',
+        country: 'Ukraine',
+        name: 'Hope of Generation',
+        src: '/images/show-7.jpg',
+        display: 'normal'
+    },
+    {
+        year: 2025,
+        city: 'Kyiv',
+        country: 'Ukraine',
+        name: 'Fantasy',
+        src: '/images/show-8.jpg',
+        display: 'reversed'
+    },
+    {
+        year: 2024,
+        city: 'Lviv',
+        country: 'Ukraine',
+        name: 'Echoes of Future Personal Showcase',
+        src: '/images/show-9.jpeg',
+        display: 'normal'
+    },
+    {
+        year: 2024,
+        city: 'Lviv',
+        country: 'Ukraine',
+        name: 'Abstraction Algorythm',
+        src: '/images/show-10.jpeg',
+        display: 'reversed'
+    },
+    {
+        year: 2024,
+        city: 'Kyiv',
+        country: 'Ukraine',
+        name: 'Sacred Amulets',
+        src: '/images/show-11.jpeg',
+        display: 'normal'
+    },
+    {
+        year: 2024,
+        city: 'Kyiv',
+        country: 'Ukraine',
+        name: 'Figurative',
+        src: '/images/show-12.jpeg',
+        display: 'reversed'
+    },
+    {
+        year: 2023,
+        city: 'Kyiv',
+        country: 'Ukraine',
+        name: 'With a Brush Through Life',
+        src: '/images/show-7.jpg',
+        display: 'normal'
     }
 ]
