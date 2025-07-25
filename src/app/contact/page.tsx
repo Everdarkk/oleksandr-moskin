3f799527d66954d968f191544be1b2f1fa901839
Oleksandr Dorokhov <alex.dorokhov.mail@gmail.com>
2025-07-25 15:11:15 +0300

    Styles improvements

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,14 @@
 import MaskedImage from "../../../lib/components/ui/MaskedImage"
 import AnimatedSocial from '../../../lib/components//AnimatedSocial'
+import Transition from "../../../lib/components/ui/Transition"
 
 export default function Contact() {
     return (
-        <section>
-            <MaskedImage src={'/images/oleksandr-3.png'}/>
-            <AnimatedSocial />
-        </section>
+        <>
+            <Transition>
+                <MaskedImage src={'/images/oleksandr-3.png'}/>
+                <AnimatedSocial />
+            </Transition>
+        </>
     )
 }
