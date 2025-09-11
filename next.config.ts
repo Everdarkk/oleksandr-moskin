1ea5cb55d16bc6537a60e0aa2bb0463e7964e691
Oleksandr Dorokhov <alex.dorokhov.mail@gmail.com>
2025-09-11 18:03:21 +0300

    next.config changed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [360, 640, 768, 1024, 1280, 1600, 1920],
+    imageSizes: [16, 32, 64, 128, 256, 512]
+  },
 };
 
 export default nextConfig;
