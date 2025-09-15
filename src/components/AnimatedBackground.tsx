b389dd6bdf48c10e27de2b988d122cb5cf8046be
Oleksandr Dorokhov <alex.dorokhov.mail@gmail.com>
2025-09-15 17:18:39 +0300

    Deploy fixes

--- src/components/AnimatedBackground.tsx
+++ src/components/AnimatedBackground.tsx
@@ -2,21 +2,22 @@
 
 import  {useDeviceDetection} from '../lib/hooks/useDeviceDetection'
 import ActiveStarfield from './ui/ActiveStarfield'
-import PassiveStarfield from './ui/PassiveStarfield'
+// import PassiveStarfield from './ui/PassiveStarfield'
 import styles from '../styles/animatedbackground.module.css'
 
 export default function AnimatedBackground() {
     const {isMobile} = useDeviceDetection()
 
-    const Starfield = isMobile ? PassiveStarfield : ActiveStarfield
+    // const Starfield = isMobile ? PassiveStarfield : ActiveStarfield
 
     return (
         <div className={styles.background}>
-            {!isMobile ? <ActiveStarfield /> : 
-            
-            <video autoPlay muted loop className={styles.video}>
-                <source src="/videos/bg-stars.mp4" type="video/mp4" />
-            </video>
+            {!isMobile ? 
+                <ActiveStarfield /> 
+                : 
+                <video autoPlay muted loop className={styles.video}>
+                    <source src="/videos/bg-stars.mp4" type="video/mp4" />
+                </video>
             }
             
 
