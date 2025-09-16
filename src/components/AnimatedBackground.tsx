5993ce3d2aacc0228b3fa7d1a8832ed3ea25be2e
Oleksandr Dorokhov <alex.dorokhov.mail@gmail.com>
2025-09-16 11:45:00 +0300

    Revert some changes in AnimatedBackground

--- src/components/AnimatedBackground.tsx
+++ src/components/AnimatedBackground.tsx
@@ -2,25 +2,17 @@
 
 import  {useDeviceDetection} from '../lib/hooks/useDeviceDetection'
 import ActiveStarfield from './ui/ActiveStarfield'
-// import PassiveStarfield from './ui/PassiveStarfield'
+import PassiveStarfield from './ui/PassiveStarfield'
 import styles from '../styles/animatedbackground.module.css'
 
 export default function AnimatedBackground() {
     const {isMobile} = useDeviceDetection()
 
-    // const Starfield = isMobile ? PassiveStarfield : ActiveStarfield
+    const Starfield = isMobile ? PassiveStarfield : ActiveStarfield
 
     return (
-        <div className={styles.background}>
-            {!isMobile ? 
-                <ActiveStarfield /> 
-                : 
-                <video autoPlay muted loop className={styles.video}>
-                    <source src="/videos/bg-stars.mp4" type="video/mp4" />
-                </video>
-            }
-            
-
+        <div className={styles.background}>    
+            <Starfield />
         </div>
   )
 }
